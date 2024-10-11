81ef30b2bd5d4db0794add3eed863bfe74b4a410
rafsun-riad <mruhaquer@gmail.com>
2024-10-11 23:57:55 +0600

    learning pathname

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -5,6 +5,7 @@ import {
 } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const navLinks = [
   {
@@ -25,6 +26,8 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
