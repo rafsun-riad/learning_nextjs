99658541d02e2a902a79cde164677294415734b3
rafsun-riad <mruhaquer@gmail.com>
2024-10-12 00:27:19 +0600

    learned usePathname

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -34,7 +36,9 @@ function SideNavigation() {
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? 'bg-primary-900' : ''
+              }`}
               href={link.href}
             >
               {link.icon}
