74b9153d6fdc0e02745aeff81c8b589861af5d0b
rafsun-riad <mruhaquer@gmail.com>
2024-10-13 00:26:38 +0600

    made cabins filter functionality

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,15 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
-
-export const revalidate = 3600;
+import Filter from '../_components/Filter';
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,9 +23,11 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
