e117d3aa23fe0f1be763a0f9666b1cb73c22a830
rafsun-riad <mruhaquer@gmail.com>
2024-10-24 15:47:05 +0600

    learned useOptimistic hook

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,33 @@
+'use client';
+
+import ReservationCard from '@/app/_components/ReservationCard';
+import { deleteReservation } from '../_lib/actions';
+import { useOptimistic } from 'react';
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currentBookings, bookingId) => {
+      return currentBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          onDelete={handleDelete}
+          booking={booking}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
